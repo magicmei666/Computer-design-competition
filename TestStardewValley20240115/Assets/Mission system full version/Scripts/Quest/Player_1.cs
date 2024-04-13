0b6f04a2b4a27f64c42915308faaeeec18304860
magicmei666 <1539290389@qq.com>
2024-04-13 19:14:21 +0800

    在Mission system full version文件夹下有任务功能相关的代码逻辑

---
+++ TestStardewValley20240115/Assets/Mission system full version/Scripts/Quest/Player_1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+//这个脚本添加在Player_1游戏对象上
+public class Player_1 : MonoBehaviour
+{
+    public static Player_1 instance;
+
+    public int exp, gold, itemAmount;
+
+    public List<Quest> questList = new List<Quest>();
+    //public Dictionary<string, Quest> questDictionary = new Dictionary<string, Quest>();//OPTIONAL
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
